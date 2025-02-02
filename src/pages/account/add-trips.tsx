1cf5242949c578058722bd2749f144bb9180d35d
Diana Aydi <you@example.com>
2025-02-02 21:25:28 +0200

    feat: add wizard layout and travel info step

--- src/pages/account/add-trips.tsx
+++ src/pages/account/add-trips.tsx
@@ -1,3 +1,9 @@
+import AddTripWizard from '@features/trip/add-trip/components/AddTripWizard';
+
 export default function AddTripPage() {
-  return <div>AddTrip</div>;
+  return (
+    <div>
+      <AddTripWizard />
+    </div>
+  );
 }
