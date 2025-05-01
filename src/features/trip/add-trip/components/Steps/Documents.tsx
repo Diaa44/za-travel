9abaa22766d16adc6a7e430a1e66f3f57b707b46
Diana Aydi <you@example.com>
2025-05-01 03:24:02 +0300

    feat : add document editing tab with auto upload

--- src/features/trip/add-trip/components/Steps/Documents.tsx
+++ src/features/trip/add-trip/components/Steps/Documents.tsx
@@ -10,13 +10,15 @@ import {
 import Pagination from '../Navigation/Pagination';
 
 export default function Documents() {
-  const { documents, onSubmit, onChange } = useDocumentsForm();
+  const { documents, onSubmit, onFileStorageRemoval, tripId } =
+    useDocumentsForm();
   return (
     <FilesForm
+      tripId={tripId}
       defaultFiles={documents}
       onSubmit={onSubmit}
       SubmitComponent={<Pagination />}
-      onChange={onChange}
+      onFileStorageRemoval={onFileStorageRemoval}
       type="document"
     />
   );
@@ -30,12 +32,13 @@ function useDocumentsForm() {
     dispatch(setDocuments(data));
     dispatch(nextStep());
   };
-  const onChange = (data: TripFile[]) => {
+  const onFileStorageRemoval = (data: TripFile[]) => {
     dispatch(setDocuments(data));
   };
   return {
     onSubmit,
     documents: trip.documents,
-    onChange,
+    onFileStorageRemoval,
+    tripId: trip.id,
   };
 }
